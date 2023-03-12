00eb6c0ffdbb6e9d4afd45b51a0348d2020027a4
Ben <jumpeebunee@gmail.com>
2023-03-12 13:50:54 +0300

    feat: add search button to map

--- src/components/AppMap.tsx
+++ src/components/AppMap.tsx
@@ -1,13 +1,12 @@
 import { YMaps, Map } from '@pbe/react-yandex-maps';
 import { API_KEY, MAP_CENTER } from '../dataConfig/dataConfig';
+import SearchBtn from './SearchBtn';
 
 const AppMap = () => {
   return (
     <YMaps query={{ apikey: API_KEY}}>
-      <Map 
-        className='app-map'
-        defaultState={MAP_CENTER}>
-
+      <Map className='app-map' defaultState={MAP_CENTER}>
+        <SearchBtn/>
       </Map>
     </YMaps>
   )
---
+++ src/components/SearchBtn.tsx
@@ -0,0 +1,9 @@
+const SearchBtn = () => {
+  return (
+    <button className="search-btn">
+      <span></span>
+    </button>
+  )
+}
+
+export default SearchBtn
