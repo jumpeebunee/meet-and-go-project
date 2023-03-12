ecb301bc3afe3e50613f257222073cfc061a48b9
Ben <jumpeebunee@gmail.com>
2023-03-12 13:58:39 +0300

    feat: add logic to search button

--- src/components/AppMap.tsx
+++ src/components/AppMap.tsx
@@ -1,12 +1,19 @@
 import { YMaps, Map } from '@pbe/react-yandex-maps';
+import { useState } from 'react';
 import { API_KEY, MAP_CENTER } from '../dataConfig/dataConfig';
 import SearchBtn from './SearchBtn';
 
 const AppMap = () => {
+
+  const [isSearchActive, setSearchActive] = useState(false);
+
   return (
     <YMaps query={{ apikey: API_KEY}}>
       <Map className='app-map' defaultState={MAP_CENTER}>
-        <SearchBtn/>
+        <SearchBtn
+          isSearchActive={isSearchActive}
+          setSearchActive={setSearchActive}
+        />
       </Map>
     </YMaps>
   )
--- src/components/SearchBtn.tsx
+++ src/components/SearchBtn.tsx
@@ -1,6 +1,16 @@
-const SearchBtn = () => {
+import { FC } from "react"
+
+interface SearchBtnProps {
+  isSearchActive: boolean;
+  setSearchActive: (value: boolean) => void;
+}
+
+const SearchBtn:FC<SearchBtnProps> = ({isSearchActive, setSearchActive}) => {
   return (
-    <button className="search-btn">
+    <button 
+      onClick={() => setSearchActive(!isSearchActive)}
+      className={isSearchActive ? 'search-btn search-btn_active' : 'search-btn'}
+    >
       <span></span>
     </button>
   )
